09b9b1255297e6166f5d50b98385cd517fa9a335
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-11-04 18:17:22 +0800

    cudacontext support raw_alloc and raw_delete instead of cudaMalloc and cudaFree

--- src/cuda_context.h
+++ src/cuda_context.h
@@ -1,6 +1,7 @@
 #ifndef DGS_CUDA_CONTEXT_H_
 #define DGS_CUDA_CONTEXT_H_
 
+#include <unordered_map>
 #include "cuda_common.h"
 
 namespace dgs {
@@ -8,12 +9,27 @@ namespace CUDAContext {
 
 class CUDAContext {
  public:
-  void Increase(int64_t nbytes) { curr_allocated_ += nbytes; }
-  void Decrease(int64_t nbytes) { curr_allocated_ -= nbytes; }
+  void _increase(int64_t nbytes) { curr_allocated_ += nbytes; }
+  void _decrease(int64_t nbytes) { curr_allocated_ -= nbytes; }
   int64_t GetCurrAllocated() { return curr_allocated_; }
+
+  void* raw_alloc(int64_t nbytes) {
+    void* p = nullptr;
+    CUDA_CALL(cudaMalloc(&p, nbytes));
+    ptr_size_dir_[reinterpret_cast<int64_t>(p)] = nbytes;
+    _increase(nbytes);
+    return p;
+  }
+
+  void raw_delete(void* p) {
+    int64_t nbytes = ptr_size_dir_[reinterpret_cast<int64_t>(p)];
+    _decrease(nbytes);
+    CUDA_CALL(cudaFree(p));
+  }
 
  private:
   int64_t curr_allocated_ = 0;
+  std::unordered_map<int64_t, int64_t> ptr_size_dir_;
 };
 
 extern CUDAContext cuda_context;
