e9a47979309ba0c6bb14323f9c34e86b948b66b5
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2023-02-14 01:22:25 +0800

    update dgl_create_communicatior

--- example/dgs_create_communicator.py
+++ example/dgs_create_communicator.py
@@ -4,13 +4,13 @@ import torch.distributed as dist
 torch.ops.load_library("./build/libdgs.so")
 
 
-def create_dgs_communicator(world_size, local_rank):
-    if local_rank == 0:
+def create_dgs_communicator(group_size, group_rank, local_group):
+    if group_rank == 0:
         unique_id_array = torch.ops.dgs_ops._CAPI_get_unique_id()
         broadcast_list = [unique_id_array]
     else:
         broadcast_list = [None]
 
-    dist.broadcast_object_list(broadcast_list, 0)
+    dist.broadcast_object_list(broadcast_list, 0, local_group)
     unique_ids = broadcast_list[0]
-    torch.ops.dgs_ops._CAPI_set_nccl(world_size, unique_ids, local_rank)
+    torch.ops.dgs_ops._CAPI_set_nccl(group_size, unique_ids, group_rank)
